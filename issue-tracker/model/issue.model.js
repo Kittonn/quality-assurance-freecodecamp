e2814b733f4fcedb665e5c6e812e41f382dec12b
Kittipod Lambangchang <tonkittipod@gmail.com>
2022-11-02 16:49:12 +0700

    feat: model & route issue tracker

--- issue-tracker/model/issue.model.js
+++ issue-tracker/model/issue.model.js
@@ -3,11 +3,12 @@ const { model, Schema } = require("mongoose");
 const issueScheme = new Schema(
   {
     assigned_to: { type: String, default: "" },
-    status_next: { type: String, default: "" },
+    status_text: { type: String, default: "" },
     open: { type: Boolean, default: true },
-    issue_title: { type: String },
-    issue_text: { type: String },
-    created_by: { type: String },
+    issue_title: { type: String, required: true },
+    issue_text: { type: String, required: true },
+    created_by: { type: String, required: true },
+    project: { type: String, required: true },
   },
   {
     timestamps: {
--- issue-tracker/routes/api.js
+++ issue-tracker/routes/api.js
@@ -1,22 +1,75 @@
 "use strict";
 
+const Issue = require("../model/issue.model");
+
 module.exports = function (app) {
   app
     .route("/api/issues/:project")
 
-    .get(function (req, res) {
-      let { project } = req.params;
+    .get(async (req, res) => {
+      const { project } = req.params;
+      req.query.project = project;
+      const issues = await Issue.find({ ...req.query });
+
+      return res.status(200).json(issues);
     })
 
-    .post(function (req, res) {
-      let { project } = req.params;
+    .post(async (req, res) => {
+      try {
+        const { project } = req.params;
+
+        if (
+          !req.body.issue_title ||
+          !req.body.issue_text ||
+          !req.body.created_by
+        ) {
+          return res.status(200).json({ error: "required field(s) missing" });
+        }
+
+        const issue = await Issue.create({
+          project,
+          ...req.body,
+        });
+
+        return res.status(200).json(issue);
+      } catch (error) {
+        console.log(error);
+      }
     })
 
-    .put(function (req, res) {
-      let { project } = req.params;
+    .put(async (req, res) => {
+      // const { project } = req.params;
+
+      // if (!req.body._id) {
+      //   return res.status(200).json({ error: "missing _id" });
+      // }
+      // if (!req.body) {
+      //   return res
+      //     .status(200)
+      //     .json({ error: "no update field(s) sent", _id: _id });
+      // }
+      // const issue = await Issue.findOneAndUpdate({ _id }, req.body, {
+      //   new: true,
+      //   runValidation: true,
+      // });
+      // if (!issue) {
+      //   return res.status(200).json({ error: "could not update", _id: _id });
+      // }
+
+      // return res.status(200).json({ result: "successfully updated", _id: _id });
     })
 
-    .delete(function (req, res) {
-      let { project } = req.params;
+    .delete(async (req, res) => {
+      // const { project } = req.params;
+      // const { _id } = req.body;
+
+      // if (!_id) {
+      //   return res.status(200).json({ error: "missing _id" });
+      // }
+      // const deleteIssue = await Issue.findOneAndDelete({ _id });
+      // if (!deleteIssue) {
+      //   return res.status(200).json({ error: "could not delete", _id: _id });
+      // }
+      // return res.status(200).json({ result: "successfully deleted", _id: _id });
     });
 };
